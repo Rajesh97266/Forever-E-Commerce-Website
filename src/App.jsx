eb825dc40951b26586ab22e4b8d4c660e2614243
Rajesh97266 <rajesh97266@gmail.com>
2024-12-26 16:11:52 +0530

    routes-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
+import Home from "./pages/Home"
+import Collection from "./pages/Collection"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import Orders from "./pages/Orders"
+import PlaceOrder from "./pages/PlaceOrder"
+
+import { Routes , Route } from 'react-router-dom'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path ="/" element={<Home/>} />
+        <Route path ="/collection" element={<Collection/>}/>
+        <Route path ="/about" element={<About/>}/>
+        <Route path ="/contact" element={<Contact/>}/>
+        <Route path ="/product/:productId" element={<Product/>}/>
+        <Route path ="/cart" element={<Cart/>}/>
+        <Route path ="/login" element={<Login/>}/>
+        <Route path ="/orders" element={<Orders/>}/>
+        <Route path ="/place-order" element={<PlaceOrder/>}/>
+
+      </Routes>
+    </div>
   )
 }
 
