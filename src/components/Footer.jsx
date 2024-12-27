e02b0cf717fe19e6f54c5f9a17eba0700b58f306
Rajesh97266 <rajesh97266@gmail.com>
2024-12-27 11:26:41 +0530

    footer-mail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,11 @@ const Footer = () => {
     <div>
       <div className="flex flex-col sm:grid sm:grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-10 text-sm  ">
         <div>
-          <img src={assets.logo} alt="logo" className="w-32 mb-5 cursor-pointer" />
+          <img
+            src={assets.logo}
+            alt="logo"
+            className="w-32 mb-5 cursor-pointer"
+          />
           <p className="w-full md:w-2/3 text-gray-600">
             At Forever, we believe in creating timeless fashion pieces that
             speak to your unique style. Whether you’re looking for everyday
@@ -17,7 +21,7 @@ const Footer = () => {
         </div>
         <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
-          <ul className="flex flex-col gap-2 cursor-pointer ">
+          <ul className="flex flex-col gap-2  ">
             <li className="text-gray-600">Home</li>
             <li className="text-gray-600">About Us</li>
             <li className="text-gray-600">Delivery</li>
@@ -28,7 +32,9 @@ const Footer = () => {
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-2 text-gray-600 ">
             <li>Phone: +123456789</li>
-            <li>Email:rajesh97266@hmail.com</li>
+            <a href="mailto:rajesh97266@gmail.com">
+              <li>Email:rajesh97266@gmail.com</li>
+            </a>
           </ul>
         </div>
       </div>
