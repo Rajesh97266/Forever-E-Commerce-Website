eba97b6c3bffa49facd276f50d875d0e33f23e86
Rajesh97266 <rajesh97266@gmail.com>
2024-12-27 09:08:06 +0530

    best-sellers-added

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -7,7 +7,7 @@ import ProductItem from "./ProductItem";
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
   const [latestProducts, setLatestProducts] = useState([]);
-  console.log(products);
+  
 
   useEffect(() => {
     setLatestProducts(products.slice(0, 10));
